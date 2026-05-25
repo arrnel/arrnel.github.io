58f5319e9402818feb6d46cde70d37e11acffa4c
Arrnel <arrnel@vk.com>
2026-05-25 04:45:09 +0300

    Add githubPageSpa plugin and base in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import githubPagesSpa from "@sctg/vite-plugin-github-pages-spa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: `/${process.env.VITE_REPO_RESUME}`,
+  plugins: [
+      react(),
+      githubPagesSpa()
+  ],
 })
