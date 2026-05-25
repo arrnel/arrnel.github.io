51258968e8e2b202f25ba03821f6a3a1e49df25b
Arrnel <arrnel@vk.com>
2026-05-25 05:00:59 +0300

    Add options to githubPagesSpa plugin and change import

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import githubPagesSpa from "@sctg/vite-plugin-github-pages-spa";
+import { githubPagesSpa } from "@sctg/vite-plugin-github-pages-spa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: `/${process.env.VITE_REPO_RESUME}`,
+  base: `/${process.env.VITE_REPO_RESUME}/`,
   plugins: [
       react(),
-      githubPagesSpa()
+      githubPagesSpa({
+          // Options are optional
+          verbose: false,
+      }),
   ],
 })
