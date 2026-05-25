939f52c1043cbb15cdc5a8ebf2b1354edc09102a
Arrnel <arrnel@vk.com>
2026-05-25 10:13:01 +0300

    Update path in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: `https://${process.env.VITE_REPO_OWNER}.github.io/${process.env.VITE_REPO_RESUME}/`,
+    base: `/${process.env.VITE_REPO_RESUME}/`,
     plugins: [
         react(),
     ],
     build: {
-        chunkSizeWarningLimit: 1600
+        chunkSizeWarningLimit: 1600,
     }
 })
